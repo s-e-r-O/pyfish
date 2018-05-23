c6b24129b8656d4b28fa3f0bed71587f4dd4ef58
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-05-23 02:36:38 -0400

    Changed unnecesary validations on LZ77 algorithm

--- lz77/lz77.py
+++ lz77/lz77.py
@@ -15,25 +15,22 @@ binMsg = binFile.read()
 binFile.close()
 compressed = b''
 l = len(binMsg)
-for i in range(0, window_size):
-    compressed += b'\x00\x00' + binMsg[i].to_bytes(length=1, byteorder='big', signed=False)
-i = window_size
+i = 0
 while i < l :
     found = False
     for j in range(i-window_size, i):
+        if (j < 0): 
+            continue
         if (binMsg[j] == binMsg[i]):
             n = 1
             found = True
             while ((i+n) < l-1 and n < 255 and binMsg[j + n] == binMsg[i + n]):
                 n += 1
-            if (n > 3):
-                compressed += (i - j).to_bytes(length=1, byteorder='big', signed=False)
-                compressed += n.to_bytes(length=1, byteorder='big', signed=False)
-                compressed += binMsg[i+n].to_bytes(length=1, byteorder='big', signed=False)
-                i+=n
-                break
-            else:
-                found = False
+            compressed += (i - j).to_bytes(length=1, byteorder='big', signed=False)
+            compressed += n.to_bytes(length=1, byteorder='big', signed=False)
+            compressed += binMsg[i+n].to_bytes(length=1, byteorder='big', signed=False)
+            i+=n
+            break
     if (not found):
         compressed += b'\x00\x00' + binMsg[i].to_bytes(length=1, byteorder='big', signed=False)
     i += 1    
